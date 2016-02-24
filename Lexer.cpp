3b4c7e6bb2ccdf5bfeb3d9e59909816fbde83140
andreaa <andrea.attard@gmail.com>
2016-02-24 21:44:37 +0100

    Added enum, struct, to Lexer.cpp
    Added enum & struct

--- Lexer.cpp
+++ Lexer.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+enum TOK_TYPE {
+    TOK_NUMBER,
+    TOK_ARITHMETIC_OP,
+    TOK_WHITESPACE,
+    TOK_COMMENT,
+    TOK_UNDEFINED,
+    TOK_EOF
+};
+
+struct Token {
+    TOK_TYPE TokenType;
+    string name;
+    int id;
+};
+
+void Token() {
+
+}
+
+void Token(TOK_TYPE type, string tokenName, int tokenID) {
+
+}
+
 Lexer::Lexer(string fileName) {
     string fileIn = "", fileInLine = "";
 
